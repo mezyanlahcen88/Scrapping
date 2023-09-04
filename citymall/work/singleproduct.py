44f03dba58aead8cbb01d5477b476409c9023dc0
unknown <mezyan.lahcen88@gmail.com>
2023-09-04 16:17:16 +0200

    updates

--- citymall/work/singleproduct.py
+++ citymall/work/singleproduct.py
@@ -17,22 +17,30 @@ product = []
 
 products_inexist_link = []
 products = []
-start = 0
-stop = 1000
+start = 290
+stop = 300
 
 def scrapSingleProduct(link):
     result = requests.get(link)
     src=result.content
     soup = BeautifulSoup(src,'html.parser')
-    product_title = soup.find('h1',{'class':'product-title product_title entry-title'}).get_text().strip()
-    product_unit = product_title.split(' ')[-1]
+    product_title = soup.find('h1',{'class':'product-title product_title entry-title'})
+    if product_title:
+        product_title = product_title.get_text().strip()
+    else :
+        "N/A"
+
+    if product_title:
+        product_unit = product_title.split(' ')[-1]
+    else :
+        "N/A"
 
     product_price = soup.find('p',{'class':'price product-page-price'})
     if product_price:
         product_price = product_price.find('bdi').get_text().split()[0]
     else :
         "N/A"
-    product_code = soup.find('span',{'class':'stl_codenum'}).get_text()
+    # product_code = soup.find('span',{'class':'stl_codenum'}).get_text()
     # product_desc = soup.find('div',{'id':'tab-description'}).get_text()
     # product_short_desc = soup.find('div',{'class':'product-short-description'})
     # if product_short_desc:
@@ -56,16 +64,16 @@ def scrapSingleProduct(link):
     item = {
         'title':product_title,
         'price':product_price,
-        'code':product_code,
-        'brand':brand,
+        # 'code':product_code,
+        'brand':product_brand,
         'unit':product_unit,
-        'category':getSSCatId(product_category ,sscats),        
+        's-s-category':product_category,        
     }
     return item
 
 for i in range(start, stop + 1):    
     products.append(scrapSingleProduct(products_globale[i]))
-    print(f"link number : + {i}")
+    print(products_globale[i])
 
 
 df=pd.DataFrame(products)
